e8ada05a73f740dea436f371dd488edd8c5b1062
IsaacAlonzo <isaac.alonzo@icloud.com>
2019-09-23 15:52:34 -0500

    new try

--- Landscape.py
+++ Landscape.py
@@ -11,11 +11,38 @@ phil.shape("classic")
 john.shape("classic")
 sven.shape("classic")
 
+bob.penup()
+phil.penup()
+sven.penup()
+
+def grass():
+    bob.begin_fill()
+    bob.color("green")
+    bob.forward(400)
+    bob.right(90)
+    bob.forward(400)
+    bob.right(90)
+    bob.forward(800)
+    bob.right(90)
+    bob.forward(400)
+    bob.right(90)
+    bob.forward(400)
+    bob.end_fill()
+
+grass()
+
+def house():
+    john.color("brown")
+    john.begin_fill()
+    for i in range(4):
+        john.forward(200)
+        john.left(90)
+    john.end_fill()
+house()
 
 
 
-
-
+john.left(90)
 
 
 
