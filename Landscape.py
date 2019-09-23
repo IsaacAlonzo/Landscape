c5ad6a52a5460a11a2cf922a4dcd3919a30dce2c
IsaacAlonzo <isaac.alonzo@icloud.com>
2019-09-23 15:22:03 -0500

    first version

---
+++ Landscape.py
@@ -0,0 +1,45 @@
+import turtle
+
+john = turtle.Turtle()
+scn = turtle.Screen()
+bob = turtle.Turtle()
+phil = turtle.Turtle()
+sven = turtle.Turtle()
+
+bob.shape("classic")
+phil.shape("classic")
+john.shape("classic")
+sven.shape("classic")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+scn.exitonclick()
